0b94e9ef34eddc1189e51727e2f39df8eab2a96a
root <root@atmradiusanddns01.snsmart.local>
2020-01-06 13:07:40 +0200

    initial commit

---
+++ build/precache-manifest.8f7dea88bf308a1949324a41257434fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308394d02cfa3b2daeb07934d11ef63c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4ed6ef307ed9d34aa59",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d0cae213083b71456c17",
+    "url": "/static/js/2.99569f39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.99569f39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4ed6ef307ed9d34aa59",
+    "url": "/static/js/main.2018dd63.chunk.js"
+  },
+  {
+    "revision": "30891cb8f3533e900f99",
+    "url": "/static/js/runtime-main.9d830519.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
